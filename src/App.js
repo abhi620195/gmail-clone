5b1662dd1ad60e5b2b51beb3e4d5e0db264c6c52
Abhishek kumar <abhi.arya97161@gmail.com>
2023-09-05 18:57:18 +0530

    compose section adde

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Emaillist from './Emaillist';
+import Compose from './Compose';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Sidebar />
         <Emaillist />
       </div>
+
+      <Compose/>
     </div>
   );
 }
